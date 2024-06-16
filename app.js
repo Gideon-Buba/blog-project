b34e81a71eb5f72072a2e7ec1671a2b8905282ca
Gideon-Buba <bubaambore@gmail.com>
2024-06-16 21:12:13 +0100

    fix:created post views and update pagination logic

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const expressLayout = require("express-ejs-layouts");
 const connectDB = require("./server/config/db");
 const PORT = 3000 || process.env.PORT;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static("public"));
 
 // Templating engine
